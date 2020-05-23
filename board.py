afec40e4a1d154d67dbb1c0b9e37cd42c18476b4
Jack Davis <jack17davis@gmail.com>
2020-05-23 11:18:56 -0400

    added full API to board.py

--- board.py
+++ board.py
@@ -1,24 +1,18 @@
 from graphics import *
 import time
 
-class Location:
-    x, y = 0,0
+pieceColors = [color_rgb(50, 50, 250), color_rgb(230, 50, 50)]
+squareColors = [color_rgb(127, 0, 0), color_rgb(255, 255, 255)] # need to make these more different
 
+class Location:
     def __init__(self,x,y):
         self.x = x
         self.y = y
+    def __str__(self):
+        return "(" + str(self.x) + "," + str(self.y) + ")"
 
 class board:
     
-    #constants
-    boardWidth = 8
-    boardHeight = 8
-    squareSize = 75# Size of each square (pixels)
-    textHeight = 50# Height of the text display at the top of the window (pixels)
-    pieceColors = [color_rgb(50, 50, 250), color_rgb(230, 50, 50)]
-    squareColors = [color_rgb(100, 220, 100), color_rgb(110, 240, 110)]
-    win = []
-
     def __init__(self, name, boardWidth = 8, boardHeight = 8, squareSize = 75):
         self.boardWidth = boardWidth
         self.boardHeight = boardHeight
@@ -29,19 +23,26 @@ class board:
         self._boardInit()
 
     def _boardInit(self):
-        flip = 1
         for i in range (0, self.boardWidth): #for every square
-            if self.boardHeight %2 == 0:
-                flip = 1 - flip
             for j in range(0,self.boardHeight):
                 top = Point(i * self.squareSize, j * self.squareSize + self.textHeight)
                 bot = Point((i + 1) * self.squareSize, (j + 1) * self.squareSize + self.textHeight)
                 temp = Rectangle(top,bot)
-                temp.setFill(self.pieceColors[flip])
-                flip = 1 - flip
+                temp.setFill(squareColors[1 if (i + j) % 2 == 1 else 0]) #alternate colors to get a checkered board
                 temp.draw(self.win)
                 #time.sleep(1)
 
+    def playersInit(self, p1sPieces, p2sPieces):
+        return 5
+
+    def makeMove(self, L1, L2):
+        return True
+
+    def clearBoard(self):
+        return 1
+
+    def setText(self,text):
+        return text
 
     def getClickedSquare(self):
         while True:
