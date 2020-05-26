cb11a0af6b7cc5de2d7f21b77bc85d31149db9b8
jack17davis <56771141+jack17davis@users.noreply.github.com>
2020-05-26 16:52:17 -0400

    still working

--- pygames.py
+++ pygames.py
@@ -1,11 +1,28 @@
-#import stdio
-from board import *
+from verticalcheckers import verticalcheckers 
 
 def prompt():
     print("choose a game to play:")
     print("0 = exit")
     print("1 = vertical checkers")
 
+def players():
+    while True:
+        print("who's playing?")
+        print("0 = exit")
+        print("1 = two humans")
+        print("2 = a human and the default computer")
+        print("3 = your player and the default computer")
+        text = input()
+        if text == str(0):
+            return 0
+        elif text == str(1):
+            return 1
+        elif text == str(2):
+            return 2
+        elif text == str(3):
+            return 3
+        else:
+            print("program was not able to handle input \"" + text + "\"")
 
 while True:
     prompt()
@@ -14,7 +31,7 @@ while True:
         break
     elif text == str(1):
         print("playing vertical checkers")
-        #verticalcheckers()
+        verticalcheckers(players())
     else:
         print("program was not able to handle input \"" + text + "\"")
 
---
+++ verticalcheckers.py
@@ -0,0 +1,24 @@
+from board import *
+import time
+
+def verticalcheckers(playercode):
+    if playercode == 0:
+        return
+    brd = board("vertical Checkers", "Player 1 = 0\t Player 2 = 0", 6, 6, 75)
+    p1sPieces = [Location(0,0),Location(1,0),Location(2,0),Location(3,0),Location(4,0),Location(5,0),
+                 Location(0,1),Location(1,1),Location(2,1),Location(3,1),Location(4,1),Location(5,1)]
+    p2sPieces = [Location(0,4),Location(1,4),Location(2,4),Location(3,4),Location(4,4),Location(5,4),
+                 Location(0,5),Location(1,5),Location(2,5),Location(3,5),Location(4,5),Location(5,5)]
+    brd.playersInit(p1sPieces, p2sPieces)
+    time.sleep(4)
+    brd.close()
+
+def humanTurn(playerToMove, )
+
+
+
+def main():
+    verticalcheckers(1)
+
+if __name__ == "__main__":
+    main()
