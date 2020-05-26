09f93c7fd801f6df7719f1a9844f7ad92ed39da4
jack17davis <56771141+jack17davis@users.noreply.github.com>
2020-05-26 13:52:04 -0400

    finished basic pygames driver

--- pygames.py
+++ pygames.py
@@ -1,11 +1,22 @@
 #import stdio
 from board import *
 
+def prompt():
+    print("choose a game to play:")
+    print("0 = exit")
+    print("1 = vertical checkers")
+
+
 while True:
+    prompt()
     text = input()
     if text == str(0):
         break
+    elif text == str(1):
+        print("playing vertical checkers")
+        #verticalcheckers()
     else:
-        print(text)
+        print("program was not able to handle input \"" + text + "\"")
 
-print("Success!")
+print("exiting...")
+
